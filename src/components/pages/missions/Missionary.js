15747ab97e324a28c61e344a9278b0427197eb0f
DelaneKay <kudakwashedelanesibanda@gmail.com>
2024-10-23 11:46:30 +0200

    Started the webpage for Clayville

--- src/components/pages/missions/Missionary.js
+++ src/components/pages/missions/Missionary.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import MissionsWelcome from './sections/MissionsWelcome/MissionsWelcome'
 
 const Missionary = () => {
   return (
     <>
-      
+      <MissionsWelcome/>
     </>
   )
 }
--- src/components/pages/missions/sections/MissionsWelcome/MissionsWelcome.js
+++ src/components/pages/missions/sections/MissionsWelcome/MissionsWelcome.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import {Row, Col} from 'react-bootstrap';
-import ClayvilleImage1 from '../../../../../media/sundayschool/Senior-Sunday-School.jpg';
-import ClayvilleImage2 from '../../../../../media/sundayschool/junior-sunday-school.jpg';
-import './SundaySchoolWelcome.css'
+import ClayvilleImage1 from '../../../../../media/missions/clayville-1.png';
+import ClayvilleImage2 from '../../../../../media/missions/clayville-2.png';
+import './MissionsWelcome.css'
 
 const MissionsWelcome = () => {
   return (
     <section>
         <Row className="align-items-center">
             <Col xs={12} md={12} lg={6} className="missions-image-col p-0">
-                <img src={JuniorImage} alt="Speaker" className="img-fluid missions-full-height-image" />
+                <img src={ClayvilleImage2} alt="Speaker" className="img-fluid missions-full-height-image" />
             </Col>
 
         {/* Right column with image and overlay */}
           <Col  xs={12} md={12} lg={6} className="missions-overlay-col p-0">
             <div className="missions-overlay-container">
-              <img src={SeniorImage} alt="Overlay background" className="img-fluid missions-full-height-image" />
+              <img src={ClayvilleImage2} alt="Overlay background" className="img-fluid missions-full-height-image" />
               <div className="missions-overlay">
                 <h1 className="missions-title">MISSIONS</h1>
               </div>
