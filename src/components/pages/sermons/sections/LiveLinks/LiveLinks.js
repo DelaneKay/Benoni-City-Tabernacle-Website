65580eaca4b04ca0ea076f205010d7f901156829
DelaneKay <kudakwashedelanesibanda@gmail.com>
2025-09-10 17:26:17 +0200

    Made Live Links responsive

--- src/components/pages/sermons/sections/LiveLinks/LiveLinks.js
+++ src/components/pages/sermons/sections/LiveLinks/LiveLinks.js
@@ -1,26 +1,70 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
+import { FaYoutube, FaGlobe, FaApple, FaAndroid } from "react-icons/fa";
 import "./LiveLinks.css"
 
 const LiveLinks = () => {
   return (
     <section className="live-links-bar">
-        <Container>
-        <Row className="justify-content-center">
-            <Col xs="auto" md={3}>
+      <Container>
+        {/* Desktop view (default) */}
+        <Row className="justify-content-center desktop-live-links">
+          <Col xs="auto" md={3}>
             <a href="https://www.youtube.com/@BenoniCityTabernacle" className="live-link">Watch Live on YouTube</a>
-            </Col>
-            <Col xs="auto" md={3}>
-            <a href="#youtube" className="live-link">Watch Live on Website</a>
-            </Col>
-            <Col xs="auto" md={3}>
-            <a href="#youtube" className="live-link">Watch Live on our Apple app</a>
-            </Col>
-            <Col xs="auto" md={3}>
-            <a href="#youtube" className="live-link">Watch Live on our Android app</a>
-            </Col>
+          </Col>
+          <Col xs="auto" md={3}>
+            <a href="#website" className="live-link">Watch Live on Website</a>
+          </Col>
+          <Col xs="auto" md={3}>
+            <a href="#apple" className="live-link">Watch Live on our Apple app</a>
+          </Col>
+          <Col xs="auto" md={3}>
+            <a href="#android" className="live-link">Watch Live on our Android app</a>
+          </Col>
         </Row>
-        </Container>
+
+        {/* Mobile view (hidden by default, only shows in media query) */}
+        <div className="mobile-live-links">
+          <h2 className="live-title">Watch Live On</h2>
+          <Row>
+            <Col xs={6} md={3}>
+              <div className="live-card">
+                <a href="https://www.youtube.com/@BenoniCityTabernacle" className="live-link">
+                  <FaYoutube className="live-icon" />
+                  <span className="live-name">YouTube</span>
+                </a>
+              </div>
+            </Col>
+
+            <Col xs={6} md={3}>
+              <div className="live-card">
+                <a href="#website" className="live-link">
+                  <FaGlobe className="live-icon" />
+                  <span className="live-name">Website</span>
+                </a>
+              </div>
+            </Col>
+
+            <Col xs={6} md={3}>
+              <div className="live-card">
+                <a href="#apple" className="live-link">
+                  <FaApple className="live-icon" />
+                  <span className="live-name">Apple App</span>
+                </a>
+              </div>
+            </Col>
+
+            <Col xs={6} md={3}>
+              <div className="live-card">
+                <a href="#android" className="live-link">
+                  <FaAndroid className="live-icon" />
+                  <span className="live-name">Android App</span>
+                </a>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Container>
     </section>
   )
 }
